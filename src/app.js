912e3ee6204a0672e46e0f3fc23c8045027464e4
romanmonfort <137888802+romanmonfort@users.noreply.github.com>
2023-10-16 22:52:52 +0000

    [dd js excusse generator and styles]

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+//La funcion que genera las escusas,no encontre otra forma que no sea con math.
+function generarExcusa() {
+  const randomQuien = who[Math.floor(Math.random() * who.length)];
+  const randomAccion = action[Math.floor(Math.random() * action.length)];
+  const randomQue = what[Math.floor(Math.random() * what.length)];
+  const randomCuando = when[Math.floor(Math.random() * when.length)];
+
+  return (
+    randomQuien + " " + randomAccion + " " + randomQue + " " + randomCuando
+  );
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excusa").innerText = generarExcusa();
 };
